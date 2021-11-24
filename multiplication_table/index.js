212d260eb230f47e23077930074218e5c9ee24c2
NicoleGitHub <94227117+NicoleGitHub@users.noreply.github.com>
2021-11-25 06:33:01 +0800

    first commit

---
+++ multiplication_table/index.js
@@ -0,0 +1,5 @@
+var string = "";
+for(var i = 1; i <= 10 ; i++){
+    string += i+"*"+i+"="+i*i+" ";
+    console.log(string);
+}
---
+++ number/index.js
@@ -0,0 +1,3 @@
+for(var i = 1; i <= 20 ; i++){ 
+    console.log(i + " is a " + ((i%2 == 1) ? "odd" : "even") + " number.");
+}
